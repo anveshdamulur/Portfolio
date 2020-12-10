b31ec2fd29aa330866a3f9908bc58558eb0cc6e9
anveshdamulur <damulurianvesh2612@gmail.com>
2020-12-10 19:45:08 +0100

    last commit

--- src/NavBar/About.js
+++ src/NavBar/About.js
@@ -13,12 +13,10 @@ function About() {
             <h1>About me</h1>
           </div>
           <div id='para'> 
-            <p> I am Anvesh Damuluri. I have completed my masters in software engineering at ISEP. I worked as an intern at Beyond. My role is Fullstack developer. Today clear in my mind to learn technical knowledge to compete in this world as well as learn interpersonal skills to develop my career in a professional path.  </p>
-          </div>
-          <div className='footer-text'>
+            <p> I am Anvesh Damuluri. I have completed my masters in software engineering at ISEP. I worked as an intern at Beyond. My role is Fullstack developer. Today clear in my mind to learn technical knowledge to compete in this world as well as learn interpersonal skills to develop my career in a professional path.  </p><br/>
             <p> I am coachable , truly hungry to learn, I am passionate to work,  I have that passion to learn, I can grasp technologies quickly,  and. If you give an opportunity to prove I have that hunger and I have that zeal in me. </p>
           </div>
-          <div>
+          <div className='download'>
               <a href={Resume} download>
                   <button value='Download'>Download Resume</button>
               </a>
