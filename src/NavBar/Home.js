c3534515b6a0a5f8799b1673e97f67a660906cad
anveshdamulur <damulurianvesh2612@gmail.com>
2021-02-12 03:05:53 +0100

    Added some functionalities

--- src/NavBar/Home.js
+++ src/NavBar/Home.js
@@ -1,19 +1,18 @@
-import profile from '../Public/Images/landing_pic.jpeg'
+import photo from '../Public/Images/raw_Photo.jpg'
 import React from 'react';
 function Home() {
   return (
 
           <div className='banner'>
-              <div className='card'>
-                  <div className='card-text'>
+              <div className='container-card'>
+                  <div className='container-card-text'>
                       <h1>Hello, I am <i>Anvesh Damuluri <span id='emoji'>;)</span></i></h1><br/>
-                      {/* <h1></h1><br/> */}
-                      <div className='card-para'>
+                      <div className='container-card-para'>
                           <span id='sub-text'> Front-End Web Developer</span>
                       </div>
                   </div>
-                  <div className='card-image'>
-                      <img src={profile} alt='img'></img>
+                  <div className='container-card-image'>
+                      <img src={photo} alt='img'></img>
                       <div className='circle'> </div>
                   </div>
               </div>
--- src/NavBar/Projects.js
+++ src/NavBar/Projects.js
@@ -1,66 +1,174 @@
-
 import React from 'react';
 function Projects() {
   return (
-    <div>
+    <>
      <h1 className='header'>Projects</h1>
-     <div className='container-project'>
-          <div className='project-data'>
-                <div className='project-data-header'>
-                    <h1>HOME EXCHANGE MANAGER (03/2019 - 06/2019)
-                        </h1>
-                </div>
-                <div className='project-data-desc'>
-                      <ul>
-                        <li>
-                              A web application that follows individuals to exchange their home
-                              for a short period of time. It was developed by using JavaEE
-                              Technologies (servelet, JSP, JSTL, EL, etc) will have to be
-                              connected to the database on the Clint side using standard
-                              technologies(HTML, CSS, JS and REACT).
-                        </li>
-                        <li> Coded 90% of client-side pages using standered technologies
-                              (HTML, CSS, REACT, JS and BootStrap). 
-                        </li>
-                        <li>led the team of 5 members.</li>
-                        <li>Drafted the project document.</li>
-                      </ul>
-                     
-                </div>
+     <div className='row ml-2 mr-2 mb-2'>
+            <div className="card-group">
+                        <div className='card'>
+                              <div className='card-header text-white' style={{backgroundColor: '#00AA9E'}}>
+                                    <div className="card-text">
+                                          <h1>Campgrounds (personal project)(04/2020 - 06/2020)</h1>
+                                    </div>
+                              </div>
+                              <div className='card-body'>
+                                    <div className='card-body card-text p-2'>
+                                          <ul>
+                                                <li>
+                                                Created a web site with login and registration pages validations.
+                                                </li>
+                                                <li> CCreated one REST Full api using NodeJS and mongoDB.
+                                                </li>
+                                                <li>The main objective of this project is to create a website and add different items to that website by using CRUD operations.</li>
+                                                <li>Technologies used : Html, CSS, Bootstrap, JavaScript, NodeJS, Express and MongoDB.ss</li>
+                                          </ul>
+                                    </div>
+                              </div>
+                              <div className="card-footer">
+                                          <button type="button" className="btn btn-primary bg-light ml-2">
+                                                <a href="https://github.com/anveshdamulur/Campgrounds/tree/master">Go To Git</a>
+                                          </button>
+                              </div>     
+                        </div>
+                  
+                 
+                        <div className='card'>
+                              <div className='card-header text-white' style={{backgroundColor: '#00AA9E'}}>
+                                    <div className="card-text">
+                                          <h1>Employee Management System (07/2020 - 09/2020)</h1>
+                                    </div>
+                              </div>
+                              <div className='card-body'>
+                                    <div className="card-text p-2">
+                                          <ul>
+                                                <li>Created a web site to perform CRUD operations.</li>
+                                                <li> This is a project majourly working on REST full routings.</li>
+                                                <li> Which is my personal project based on technologies I have learnt recently</li>
+                                                <li>This project is completely developed by using HTML, CSS, JavaScript, NodeJs, Express EJS and MongoDb Atlas.</li>
+                                                <li>Tools I have used for this project are visual studio, postman, github.</li>
+                                          </ul>     
+                                    </div>      
+                              </div>
+                             
+                              <div className="card-footer">
+                                          <button type="button" className="btn btn-primary bg-light">
+                                                <a href="https://github.com/anveshdamulur/EmployeeManagement">Go To Git</a>
+                                          </button>
+                              </div>  
+                        </div>
+                  
+                        <div className='card'>
+                              <div className='card-header text-white' style={{backgroundColor: '#00AA9E'}}>
+                              <div className="card-text">
+                                    <h1>HOME AUTOMATION NETWORK (10/2018 - 01/2019)</h1>
+                              </div>
+                              </div>
+                              <div className='card-body'>
+                                    <div className="card-text p-2">
+                                          <ul>
+                                                <li>Web app for a home automation system that controls light, temperature and more. It typically connects controlled devices to
+                                                a central hub or gateway. As a web developer. </li>
+                                                <li>my roles were as follows:</li>
+                                                <li>The main objective of this project is to create a website and add
+                                                different items to that website by using CRUD operations.</li>
+                                                <li>Technologies used : Html, CSS, Bootstrap, JavaScript, NodeJS, Express and MongoDB.</li>
+                                          </ul>
+                                    </div>
+
+                              </div>
+                        </div>
+                  
             </div>
          
-          <div className='project-data'>
-                <div className='project-data-header'>
-                    <h1>Employee Management System (07/2020 - 09/2020)</h1>
-                </div>
-                <div className='project-data-desc'>
-                      <ul>
-                        <li>Created a web site to perform CRUD operations.</li>
-                        <li> This is a project majourly working on REST full routings.</li>
-                        <li> Which is my personal project based on technologies I have learnt recently</li>
-                        <li>This project is completely developed by using HTML, CSS, JavaScript, NodeJs, Express EJS and MongoDb Atlas.</li>
-                        <li>Tools I have used for this project are visual studio, postman, github.</li>
-                      </ul>
-                </div>
-            </div>
-          
-          <div className='project-data'>
-                <div className='project-data-header'>
-                    <h1>HOME AUTOMATION NETWORK (10/2018 - 01/2019)</h1>
-                </div>
-                <div className='project-data-desc'>
-                      <ul>
-                        <li>Web app for a home automation system that controls light, temperature and more. It typically connects controlled devices to
-                        a central hub or gateway. As a web developer. </li>
-                        <li>my roles were as follows:</li>
-                        <li>The main objective of this project is to create a website and add
-                            different items to that website by using CRUD operations.</li>
-                        <li>Technologies used : Html, CSS, Bootstrap, JavaScript, NodeJS, Express and MongoDB.</li>
-                      </ul>
-                </div>
+     </div>
+     <div className='row ml-2 mr-2 mb-2'>
+            <div className="card-group">          
+                        <div className="card">
+                              <div className="card-header text-white" style={{backgroundColor: '#00AA9E'}}>
+                                    <div className="card-text">
+                                          <h1>Hotel Application</h1>
+                                    </div>
+                              </div>
+                              <div className="card-body">
+                                    <div className="card-text p-2">
+                                          <ul>
+                                                      <li>This is one Hotel Application developed by using ReactJS.</li>
+                                                      <li>Main functions are:</li> 
+                                                      <li>Get the data from API using fetch, Axios and async functions.</li>
+                                                      <li>Display the data using Cards.</li>
+                                                      <li>Using Filters to get the data acoording to some given inputs.</li>
+                                                      <li>Used React-Router to Link the pages.</li>
+                                           </ul>
+                                    </div>
+                              </div>
+                              <div className='card-title p-3'>
+                                    <p style={{fontWeight:'bold', fontSize:'20px'}}>Do you want to visit my web site?
+                                     Why are you waiting !!! Here it is..</p>
+                              </div>
+                              <div class="card-footer">
+                                          <button type="button" className="btn btn-primary bg-light">
+                                                <a href="https://react-hotel-application.herokuapp.com/">Go To Live</a>
+                                          </button>
+                              </div>
+                        </div>
+                        <div className="card">
+                              <div className="card-header text-white" style={{backgroundColor: '#00AA9E'}}>
+                                    <div className="card-text">
+                                          <h1>Space-X Application</h1>
+                                    </div>
+                              </div>
+                              <div className="card-body">
+                                    <div className="card-text p-2">
+                                          <ul>
+                                                <li>It's a Single Web Application created by using ReactJS.</li>
+                                                <li>In this application I have taken api from space-x.</li>
+                                                <li>By using Axios get the data from Api and displayed according to the id.</li>
+                                                <li>Used React slide show to display slide images.</li>
+                                                <li>Pushed into Git repository.</li>
+                                                <li>Finally Deployed in heroku website.</li>
+                                          </ul> 
+                                    </div>
+                                    
+                              </div>
+                              <div className='card-title p-3'>
+                                    <p style={{fontWeight:'bold', fontSize:'20px'}}>Do you want to visit my web site?
+                                     Why are you waiting !!! Here it is..</p>
+                              </div>
+                              <div className="card-footer">
+                                          <button type="button" className="btn btn-primary bg-light">
+                                                <a href="https://space-x-awesome-app.herokuapp.com/">Go to Live</a>
+                                          </button>
+                              </div>  
+                        </div>
+                        <div className="card">
+                              <div className="card-header text-white" style={{backgroundColor: '#00AA9E'}}>
+                                    <div className="card-text">
+                                          <h1>React Slide Show App</h1>
+                                    </div>
+                              </div>
+                              <div className="card-body">
+                                    <div className="card-text p-2">
+                                          <li>Its a pure react-slide-show application</li>
+                                          <li>This is a small test application to perform slide functionality.</li>
+                                          <li>In this test I have used CSS3 to make It style and also responsive.</li>
+                                          <li>Maintained code in Git Repo.</li>
+                                          <li>Live application is in Heroku server.</li>
+                                    </div>      
+                              </div>
+                              <div className='card-title p-3'>
+                                    <p style={{fontWeight:'bold', fontSize:'20px'}}>Do you want to visit my web site?
+                                     Why are you waiting !!! Here it is..</p>
+                              </div>
+                              <div className="card-footer">
+                                    <button type="button" className="btn btn-primary bg-light">
+                                          <a href="https://loumireactslideshowapp.herokuapp.com/">Go to Live</a>
+                                    </button>
+                              </div>
+                        </div>
+                  
             </div>
      </div>
-    </div>
+    </>
   );
 }
 
